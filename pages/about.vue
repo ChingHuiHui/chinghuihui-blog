32ee62564467a16974527f22a9a8b1a45fee3da9
chinghuihui <sunny70624@gmail.com>
2022-06-12 19:28:16 +0800

    about page

---
+++ pages/about.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="mx-auto w-fit">
+    <img
+      class="w-52 rounded-full mx-auto mb-6"
+      src="~/assets/image/about-hui.jpg"
+      alt=""
+    />
+    <div class="md:text-lg text-center flex flex-col items-center">
+      <div class="mb-4 space-y-1">
+        <div class="font-semibold text-xl md:text-3xl">
+          Ching Hui Chang / 張晴惠
+        </div>
+        <div>New Taipei City, Taiwan. 23y</div>
+        <div class="underline">Frontend Developer</div>
+        <div>FJUIM, GPA:3.94</div>
+      </div>
+      <p
+        class="text-base px-4 py-2 border mb-10 text-left space-y-4 max-w-[28rem]"
+      >
+        <span class="inline-block">
+          I started programming in my freshman year, and keep learning
+          knowledge, I want to be a more strong developer. :)
+        </span>
+        <span class="inline-block">
+          In addition to programming, I love playing with my friends, also I'm a
+          fangirl!!
+        </span>
+      </p>
+      <div>
+        <ul class="flex items-center space-x-4">
+          <li>
+            <a target="_blank" href="https://github.com/ChingHuiHui"
+              ><img src="~/assets/image/icon/github.svg" alt=""
+            /></a>
+          </li>
+          <li>
+            <a target="_blank" href="https://twitter.com/chinghuihui_126"
+              ><img src="~/assets/image/icon/twitter.svg" alt=""
+            /></a>
+          </li>
+          <li>
+            <a target="_blank" href="https://www.instagram.com/sunnyhui_0126/"
+              ><img src="~/assets/image/icon/instagram.svg" alt=""
+            /></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+ul img {
+  @apply w-6;
+}
+</style>
